4db608913dabf461b87dad84ba62079ae3c7e590
galoryzen <rjlopezgr@gmail.com>
2024-10-21 18:14:25 -0500

    change data source

--- db/data-source.ts
+++ db/data-source.ts
@@ -6,15 +6,16 @@ export const dataSourceOptions: DataSourceOptions = {
     type: 'postgres',
     host: process.env.DB_HOST || 'localhost',
     port: 5432,
-    username: 'postgres',
-    password: 'postgres',
-    database: 'arte7',
+    username: process.env.DB_USERNAME || 'postgres',
+    password: process.env.DB_PASSWORD || 'postgres',
+    database: process.env.DB_DATABASE || 'arte7',
     entities: ['dist/**/*.entity.js'],
-    dropSchema: true,
-    synchronize: true,
+    dropSchema: false,
+    synchronize: false,
     migrations: ['dist/shared/migrations/**/*{.ts,.js}'],
-    migrationsRun: false,
+    migrationsRun: true,
 };
+
 
 const dataSource = new DataSource(dataSourceOptions);
 export default dataSource;
